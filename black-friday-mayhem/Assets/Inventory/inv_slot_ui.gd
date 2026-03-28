bdeb5f6bbf4c9bc31ad6e9e20c95819247bd89e2
bluedez <hernandez.profreshional@gmail.com>
2026-03-27 21:38:48 -0600

    added item descriptions to inventory

--- black-friday-mayhem/Assets/Inventory/inv_slot_ui.gd
+++ black-friday-mayhem/Assets/Inventory/inv_slot_ui.gd
@@ -1,12 +1,18 @@
 extends Panel
 
 @onready var itemSprite: Sprite2D = $CenterContainer/Panel/ItemDisplay
-@onready var stack_text: Label = $CenterContainer/Panel/Label
+@onready var stack_text: Label = $CenterContainer/Panel/StackNum
+@onready var itemTag: Panel = $ItemTag
+@onready var itemName = $ItemTag/ItemName
+@onready var itemDesc = $ItemTag/ItemDesc
+var slotGlobal: InvSlot
 
 func update(slot: InvSlot):
+	slotGlobal = slot
 	if not slot.item:
 		itemSprite.visible = false
 		stack_text.visible = false
+		itemTag.visible = false
 	else:
 		itemSprite.visible = true
 		itemSprite.texture = slot.item.sprite
@@ -14,3 +20,12 @@ func update(slot: InvSlot):
 		if slot.amount > 1:
 			stack_text.visible = true
 		stack_text.text = str(slot.amount)
+
+func _on_mouse_hover():
+	if slotGlobal.item:
+		itemName.text = slotGlobal.item.name
+		itemDesc.text = slotGlobal.item.itemDesc
+		itemTag.visible = true
+	
+func _on_mouse_leave():
+	itemTag.visible = false
