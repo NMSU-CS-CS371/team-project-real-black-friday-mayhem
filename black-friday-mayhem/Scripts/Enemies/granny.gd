88452aa633b276cebb53985189206286fcf44638
Samiel T <samielthai@gmail.com>
2026-04-21 02:18:59 -0600

    update Scalper, Karen, Granny scenes and scripts to use EntityMovement base class for collision fix

--- black-friday-mayhem/Scripts/Enemies/granny.gd
+++ black-friday-mayhem/Scripts/Enemies/granny.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends EntityMovement
 
 #movement varibles
 var time = 0.0
@@ -8,7 +8,6 @@ var angle = 0.0
 var center : Vector3
 @export var game : Node3D
 var last_position = Vector3.ZERO
-@onready var sprite = $AnimatedSprite3D
 @onready var player = get_tree().root.get_child(1).player
 var is_defeated = false
 var playing_game = false
@@ -19,113 +18,114 @@ var hit = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#get positions ready
-	last_position = global_position
 	center = global_position  # starting point = center of circle
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if is_defeated :
-		return
+		velocity = Vector3.ZERO
 	elif playing_game :
-		$AnimatedSprite3D.play("Final")
+		sprite.play("Final")
+		velocity = Vector3.ZERO
 	else :
-		$AnimatedSprite3D.offset.y = 15
+		sprite.offset.y = 15
 		#-move-
 		time += delta * speed
-		
+
 		#calculate new position
 		var new_x = center.x + cos(time) * radius
 		var new_z = center.z + sin(time) * radius
-		
-		global_position.x = new_x
-		global_position.z = new_z
-		
-		update_animation()
-		last_position = global_position
-		
-func _physics_process(_delta: float) -> void:
-	if $RayCast3D.is_colliding():
-		var floor_height = $RayCast3D.get_collision_point().y
-		global_position.y = floor_height
-		#jolena, we don't need this... :face_palm:
-		#(if you place them from the folder to the floor, it will just snap in general...)
-		
-		#what if it does not?
-		#(i.e. too small a raycast to hit the floor or the floor is above the enemy
-		#plus! It uses too many resources!!
-		# I would get it if we only did this once, but it's every frame! We only need to check twice!!
-		#(unless we're making [WAFFLES] stairs, then I could definitely see the use...)
+		var target_pos = Vector3(new_x, global_position.y, new_z)
 
-func play_if_not(anim, flip):
+		var direction = (target_pos - global_position)
+		direction.y = 0
+		velocity = direction / delta
+
+	# velocity + apply_movement_and_animation() handles grounding now
+	# no more per-frame raycasting :)
+
+	apply_movement_and_animation(delta)
+
+# Override update_animation since Granny uses specific hit-based animations
+func play_if_not_flip(anim, flip):
 	if sprite.animation != anim:
-		if flip :
-			sprite.flip_h = true
-		else :
-			sprite.flip_h = false
+		sprite.flip_h = flip
 		sprite.play(anim)
-		
-			
 
-func update_animation() :
-	var direction = global_position - last_position
-	
+
+
+func update_animation() -> void:
+	var direction = velocity	# global_position - last_position = velocity
+	direction.y = 0				# ignore vertical movement for animation purposes
+
 	#prevents tiny jittering movements
 	if direction.length() < 0.001 :
 		return
-		
+
 	#normalize the direction
 	direction = direction.normalized()
-	
+
 	#decide animation
 	if abs(direction.x) > abs(direction.z) :
 		if direction.x > 0 :
 			if hit == 0 :
-				play_if_not("Side1", true)
-			if hit == 1 :
-				play_if_not("Side2", true)
-			if hit >= 2 :
-				play_if_not("Side3", true)
+				play_if_not_flip("Side1", true)
+			elif hit == 1 :
+				play_if_not_flip("Side2", true)
+			elif hit >= 2 :
+				play_if_not_flip("Side3", true)
 		else :
 			if hit == 0 :
-				play_if_not("Side1", false)
-			if hit == 1 :
-				play_if_not("Side2", false)
-			if hit >= 2 :
-				play_if_not("Side3", false)
+				play_if_not_flip("Side1", false)
+			elif hit == 1 :
+				play_if_not_flip("Side2", false)
+			elif hit >= 2 :
+				play_if_not_flip("Side3", false)
 	else :
 		if direction.z > 0 :
 			if hit == 0 :
-				play_if_not("Front1", false)
-			if hit == 1 :
-				play_if_not("Front2", false)
-			if hit >= 2 :
-				play_if_not("Front3", false)
+				play_if_not_flip("Front1", false)
+			elif hit == 1 :
+				play_if_not_flip("Front2", false)
+			elif hit >= 2 :
+				play_if_not_flip("Front3", false)
 		else :
-			play_if_not("Back", false)
-			
+			play_if_not_flip("Back", false)
+
 
 func _on_hit_box_body_entered(body: Node3D) -> void:
-	if hit >= 2 && !player.playing_game:
+	# Ensure the player exists and has stop_velocity signal
+	if not body.is_in_group("Player"):
+		return
+
+	# Temporary fallback if player variable isn't fully set
+	var game_active = false
+	if player and "playing_game" in player:
+		game_active = player.playing_game
+
+	# Check if the player is currently playing the minigame before processing the hit
+	if hit >= 2 && !game_active:
 		print("oh heck no")
-		player.playing_game = true
-		body.stop_velocity.emit()
+		if player and "playing_game" in player:
+			player.playing_game = true
+		if body.has_signal("stop_velocity"):
+			body.stop_velocity.emit()
 		var killer_instinct = punchies.instantiate()
 		add_child(killer_instinct)
 	else:
 		hit = hit + 1
 		print("sorry.!")
-		
+
 
 #this function is for the textbox when grandma is hit
 func when_hit(_num) :
 	pass
-	#when the granny is hit I want to have you and the grandma stop 
+	#when the granny is hit I want to have you and the grandma stop
 	#and a text box of the grandma and you to be locked in
 	#until you tap/press a button
 	#wont increase granny hit until text box is closed
-	
+
 	#TO DO:
-	#write comment that make sense pls ^ 
-		
+	#write comment that make sense pls ^
+
--- black-friday-mayhem/Scripts/Enemies/karen.gd
+++ black-friday-mayhem/Scripts/Enemies/karen.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends EntityMovement
 
 #movement varibles
 @export var speed := 1.0
@@ -7,26 +7,16 @@ extends Node3D
 var moving = true
 var time = 0.0
 var radius = 5.0
-#var speed = 1.0
 var angle = 0.0
 var center : Vector3
-var last_position = Vector3.ZERO
-@onready var sprite = $AnimatedSprite3D
 
 #mini game varibles
 var is_defeated = false
 var playing_game = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	last_position = global_position
 	center = global_position  # starting point = center of circle
 	cycle()
-
-func _physics_process(_delta: float) -> void:
-	if $RayCast3D.is_colliding():
-		var floor_height = $RayCast3D.get_collision_point().y
-		global_position.y = floor_height
 
 func cycle():
 	while true:
@@ -35,57 +25,37 @@ func cycle():
 		await get_tree().create_timer(move_time).timeout
 		# Pause phase
 		moving = false
-		sprite.play("idle")
+		if not is_defeated and not playing_game:
+			sprite.play("idle")
 		await get_tree().create_timer(pause_time).timeout
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if is_defeated :
-		#$AnimatedSprite3D.play("defeated")
 		sprite.offset.y = 7
+		velocity = Vector3.ZERO
 	elif playing_game :
 		sprite.play("idle")
+		velocity = Vector3.ZERO
 	else :
 		sprite.offset.y = 15
 		if moving:
 			#-move-
-			#time += delta * speed
 			angle += speed * delta
 			
 			#calculate new position
 			var new_x = center.x + cos(angle) * radius
 			var new_z = center.z + sin(angle) * radius
-			global_position = Vector3(new_x, global_position.y, new_z)
-			update_animation()
-			last_position = global_position
+			var target_pos = Vector3(new_x, global_position.y, new_z)
+			
+			var direction = (target_pos - global_position)
+			direction.y = 0
+			velocity = direction / delta
+		else:
+			velocity = Vector3.ZERO
+			
+	apply_movement_and_animation(delta)
 	
-func update_animation() :
-	var direction = global_position - last_position
-	
-	#prevents tiny jittering movements
-	if direction.length() < 0.001 :
-		return
-		
-	#normalize the direction
-	direction = direction.normalized()
-	
-	#decide animation
-	if abs(direction.x) > abs(direction.z) :
-		if direction.x > 0 :
-			play_if_not("Right")
-		else :
-			play_if_not("Left")
-	else :
-		if direction.z > 0 :
-			play_if_not("Forward")
-		else :
-			play_if_not("Back")
-
-
-func play_if_not(anim):
-	if sprite.animation != anim:
-		sprite.play(anim)
+# moved movement and animation logic to entity_movement.gd
 
 func _on_hit_box_body_entered(_body: Node3D) -> void:
 	print("karen hit")
-	
