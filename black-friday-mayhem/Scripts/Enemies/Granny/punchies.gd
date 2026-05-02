190c3c6a6ece3a35702abd6afd0cf256e8a2c702
Jaiden Covarrubias <jaidenc@nmsu.edu>
2026-05-01 19:39:10 -0600

    grandma has candy

--- black-friday-mayhem/Scripts/Enemies/Granny/punchies.gd
+++ black-friday-mayhem/Scripts/Enemies/Granny/punchies.gd
@@ -1,7 +1,11 @@
 extends Node2D
-
+@onready var player = get_tree().get_first_node_in_group("Player")
 signal game_finished(result)
 
 func emit_gamestate(win_state : String):
+	var item : InvItem
+	item = preload("res://Assets/Inventory/Items/strawberrybanbans.tres")
+	player.inventory.insert(item)
+	player.inventory.beatGrandma = true
 	emit_signal("game_finished", win_state)
 	pass
